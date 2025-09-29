fa1216094171b3a71c5b418ae4a17c76c529b989
Yurii Osipov <jeetforskill@gmail.com>
2025-09-29 15:31:55 +0300

    Додав функціонал кнопки вверх

--- init.js
+++ init.js
@@ -2,10 +2,12 @@ import { switchTheme } from "./src/components/switchTheme.js";
 import { getGeoData } from "./src/API/geoData.js";
 import { getWeatherByForm } from "./src/components/inputForm.js";
 import { renderCurrentTime } from "./src/healpers/currentTime.js";
+import { scrollToTop } from "./src/components/scrollToTop.js";
 
 export function initApp() {
   switchTheme();
   getGeoData();
   getWeatherByForm();
   renderCurrentTime();
+  scrollToTop();
 }
