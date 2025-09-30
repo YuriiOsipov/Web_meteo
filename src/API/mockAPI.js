2ca16846e4285529727b14cf9bb77df3875a0074
Yurii Osipov <jeetforskill@gmail.com>
2025-09-30 14:52:57 +0300

    Додав мокову заглушку для динамічного рендерингу

---
+++ src/API/mockAPI.js
@@ -0,0 +1,49 @@
+export const stubData = {
+  coord: {
+    lon: 30.5241,
+    lat: 50.45,
+  },
+  weather: [
+    {
+      id: 804,
+      main: "Clear",
+      description: "чисте небо",
+      icon: "01d",
+    },
+  ],
+  base: "stations",
+  main: {
+    temp: 25,
+    feels_like: 24,
+    temp_min: 25,
+    temp_max: 25,
+    pressure: 1021,
+    humidity: 60,
+    sea_level: 1021,
+    grnd_level: 910,
+  },
+  visibility: 10000,
+  wind: {
+    speed: 4.09,
+    deg: 45,
+    gust: 3.47,
+  },
+  rain: {
+    "1h": 2.73,
+  },
+  clouds: {
+    all: 100,
+  },
+  dt: 1759231085,
+  sys: {
+    type: 1,
+    id: 6736,
+    country: "UA",
+    sunrise: 1759204582,
+    sunset: 1759246760,
+  },
+  timezone: 10800,
+  id: 696050,
+  name: "Pushcha-Vodytsya",
+  cod: 200,
+};
